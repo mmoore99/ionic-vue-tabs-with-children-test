3d6ead1c8c2f71d12e84a84bf07d6a7049b731b9
Michael Moore <mike@fiddlebits.net>
2023-06-24 11:47:54 -0700

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.vued.tabs.with.children.test',
+  appName: 'ionic-vued-tabs-with-children-test',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
